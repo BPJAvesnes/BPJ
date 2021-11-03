7ea521a01f0ef0b775ccb7eac7ef210a5d18ef51
Vitrant Clément <91595309+CVitrant@users.noreply.github.com>
2021-11-03 14:44:36 +0100

    js in html transferred in js file

---
+++ site/js/script.js
@@ -0,0 +1,73 @@
+
+			let items = document.querySelectorAll('[id^="recipeCarousel"] .carousel-item')
+
+            items.forEach((el) => {
+                const minPerSlide = 4
+                let next = el.nextElementSibling
+                for (var i=1; i<minPerSlide; i++) {
+                    if (!next) {
+                        // wrap carousel by using first child
+        	           next = items[0]
+      	         }
+                    let cloneChild = next.cloneNode(true)
+                    el.appendChild(cloneChild.children[0])
+                    next = next.nextElementSibling
+        }
+})
+
+		
+
+
+function myFuLan() {
+				  var x = document.getElementById("contLan");
+				  var y = document.getElementById("contAve");
+				  var z = document.getElementById("contFou");
+				  if (x.style.display === "block") {
+					x.style.display = "none";
+					y.style.display = "none";
+					z.style.display = "none";
+					  
+				}
+				 else {
+					y.style.display = "none";
+					z.style.display = "none";
+					x.style.display = "block";
+					  
+				}
+			}
+            
+			function myFuAve() {
+				  var x = document.getElementById("contAve");
+				  var y = document.getElementById("contLan");
+				  var z = document.getElementById("contFou");
+				  if (x.style.display === "block") {
+					x.style.display = "none";
+					y.style.display = "none";
+					z.style.display = "none";
+					  
+				}
+				 else {
+					y.style.display = "none";
+					z.style.display = "none";
+					x.style.display = "block";
+					  
+				}
+			}
+            
+			function myFuFou() {
+				  var x = document.getElementById("contFou");
+				  var y = document.getElementById("contLan");
+				  var z = document.getElementById("contAve");
+				  if (x.style.display === "block") {
+					x.style.display = "none";
+					y.style.display = "none";
+					z.style.display = "none";
+					  
+				}
+				 else {
+					y.style.display = "none";
+					z.style.display = "none";
+					x.style.display = "block";
+					  
+				}
+			}
