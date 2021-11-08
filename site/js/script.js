68cd5e5e8e9b73a939be465fbc22597ec5f14a1b
Vitrant Clément <91595309+CVitrant@users.noreply.github.com>
2021-11-08 16:45:45 +0100

    added zipcode arrays in js

--- site/js/script.js
+++ site/js/script.js
@@ -92,12 +92,18 @@ zipInput.addEventListener('invalid', () => {
 let zipLan = [481,006,031,053,057,065,070,077,099,116,164,194,217,223,232,242,246,251,259,265,277,283,296,310,315,323,325,331,353,357,363,381,384,396,425,441,451,464,468,472,473,494,503,518,548,549,565,584,607,619,626,639,640];
 let lenLan = zipLan.length;
 
+let zipAve = [036,035,050,061,062,066,078,093,134,147,148,169,174,175,177,181,186,218,226,233,240,241,270,290,306,333,342,347,374,461,474,490,493,525,529,534,555,562,563,572,573,583,649];
+let lenAve = zipAve.length;
+
+let zipFou = [249,012,045,198,229,261,420,445,601,633,659,661];
+let lenFou = zipFou.length;
+
+
+
 function zipVerify() {
     let x = document.getElementById("zipCode").value;
-    for (var i = 0; i < lenLan; i++) {
-    console.log(sequence[i]);;
-    }
-}
+    document.getElementById("zipResult").innerHTML = x;
+};
 
 
 
