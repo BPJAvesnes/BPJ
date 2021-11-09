67629b9a979e7068c469b6495d1f533b3cf3d5c4
Vitrant Clément <91595309+CVitrant@users.noreply.github.com>
2021-11-09 11:23:36 +0100

    started working with vicopo
    vicopo files + jquery added to website files to simplify zipcode and city name input

--- site/js/script.js
+++ site/js/script.js
@@ -1,10 +1,10 @@
 
-			let items = document.querySelectorAll('[id^="recipeCarousel"] .carousel-item')
+let items = document.querySelectorAll('[id^="recipeCarousel"] .carousel-item')
 
-            items.forEach((el) => {
-                const minPerSlide = 4
-                let next = el.nextElementSibling
-                for (var i=1; i<minPerSlide; i++) {
+items.forEach((el) => {
+        const minPerSlide = 4
+        let next = el.nextElementSibling
+         for (var i=1; i<minPerSlide; i++) {
                     if (!next) {
                         // wrap carousel by using first child
         	           next = items[0]
@@ -13,7 +13,7 @@
                     el.appendChild(cloneChild.children[0])
                     next = next.nextElementSibling
         }
-})
+});
 
 		
 
@@ -34,7 +34,7 @@ function myFuLan() {
 					x.style.display = "block";
 					  
 				}
-			}
+			};
             
 			function myFuAve() {
 				  var x = document.getElementById("contAve");
@@ -52,7 +52,7 @@ function myFuLan() {
 					x.style.display = "block";
 					  
 				}
-			}
+			};
             
 			function myFuFou() {
 				  var x = document.getElementById("contFou");
@@ -70,24 +70,9 @@ function myFuLan() {
 					x.style.display = "block";
 					  
 				}
-			}
+			};
 
 /* zipcode search*/
-
-const zipInput = document.getElementById("zipCode");
-
-zipInput.addEventListener('input', () => {
-  zipInput.setCustomValidity('');
-  zipInput.checkValidity();
-});
-
-zipInput.addEventListener('invalid', () => {
-  if(zipInput.value === '') {
-    zipInput.setCustomValidity("Veuillez saisir votre nom d'utilisateur !");
-  } else {
-    zipInput.setCustomValidity("Un nom d'utilisateur ne peut contenir que des lettres minuscules et majuscules, veuillez réessayer");
-  }
-});
 
 let zipLan = [481,006,031,053,057,065,070,077,099,116,164,194,217,223,232,242,246,251,259,265,277,283,296,310,315,323,325,331,353,357,363,381,384,396,425,441,451,464,468,472,473,494,503,518,548,549,565,584,607,619,626,639,640];
 let lenLan = zipLan.length;
@@ -98,12 +83,23 @@ let lenAve = zipAve.length;
 let zipFou = [249,012,045,198,229,261,420,445,601,633,659,661];
 let lenFou = zipFou.length;
 
-
-
 function zipVerify() {
     let x = document.getElementById("zipCode").value;
-    document.getElementById("zipResult").innerHTML = x;
-};
+    if ( x >= 59000 && x <= 59999) {
+        if (zipLan.includes(x - 59000)) {
+            document.getElementById("zipResult").innerHTML = ("Vous êtes rattaché à la Communauté de Commune du Pays de Mormal, veuillez vous rapprocher du Centre Social et Culturel de Landrecies");
+        } else if (zipAve.includes(x - 59000)){
+            document.getElementById("zipResult").innerHTML = ("Vous êtes rattaché à la Communauté de communes Cœur de l'Avesnois, veuillez vous rapprocher du Centre Social et Culturel d'Avesnes-sur-Helpe");
+            } else if (zipFou.includes(x - 59000)) {
+            document.getElementById("zipResult").innerHTML = ("Vous êtes rattaché à la Communauté de communes du Sud Avesnois, veuillez vous rapprocher du Centre Social et Culturel de Fourmies");
+                } else {
+                    document.getElementById("zipResult").innerHTML =  ("Votre commune n'est pas rattachée aux communautés de communes proposant la BPJ");
+            }
+            
+    } else {
+        document.getElementById("zipResult").innerHTML =  ("Le code postal rentré n'est pas situé dans le nord, redirigez vous vers la BPJ la plus proche de chez vous");
+    }
+}
 
 
 
