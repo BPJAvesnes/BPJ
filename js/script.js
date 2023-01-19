676712c876c72025db934e2a4facc5aa443acb24
pupitoe <49617955+pupitoe@users.noreply.github.com>
2023-01-19 16:51:20 +0100

    changement OWO

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 /*multi-item carousel on gallery page*/
 
-
+// ici ya surment le bug de merde sur le scrooling
 let items = document.querySelectorAll('[id^="recipeCarousel"] .carousel-item');
 
 items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
+    const minPerSlide = 4;
+    let next = el.nextElementSibling;
     for (var i = 1; i < minPerSlide; i++) {
         if (!next)
         {
             // wrap carousel by using first child
-            next = items[0]
+            next = items[0];
         }
 
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
+        let cloneChild = next.cloneNode(true);
+        el.appendChild(cloneChild.children[0]);
+        next = next.nextElementSibling;
     }
 });
 
